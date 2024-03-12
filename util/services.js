c9682f3537d635efff99dc82a3497dc5454dca2a
Stan <egbestanley84@yahoo.com>
2024-03-12 10:08:41 +0000

    feat: calculate course summary statistics.

--- util/services.js
+++ util/services.js
@@ -7,8 +7,36 @@ const FOLDER = 'json_data'
 const FILE = 'course-grades-data.json'
 
 module.exports = {
-    generateGradesFile
-  }
+    generateGradesFile,
+    calculateCourseStats
+}
+
+async function calculateCourseStats(courseGradesData) {
+    const statistics = {}
+    courseGradesData.forEach(({ course, grade }) => {
+        if (!statistics[course]) {
+            statistics[course] = {
+                highestGrade: 0,
+                lowestGrade: 0,
+                totalGrades: 0,
+                count: 0
+            }
+        }
+        const courseStats = statistics[course]
+        courseStats.highestGrade = Math.max(courseStats.highestGrade, grade)
+        courseStats.lowestGrade = Math.min(courseStats.lowestGrade, grade)
+        courseStats.totalGrades += grade
+        courseStats.count++
+    })
+
+    Object.keys(statistics).forEach(course => {
+        const courseStats = statistics[course]
+        courseStats.averageGrade = courseStats.totalGrades / courseStats.count
+        delete courseStats.totalGrades
+        delete courseStats.count
+    })
+    return statistics
+}
 
 async function generateGradesFile() {
     try {
