c83fb2abdbf0b95384930ba8c5471c352e511af6
Stan <egbestanley84@yahoo.com>
2024-03-12 09:37:33 +0000

    feat: creates directory and download json data on db-init

--- fetch-db.js
+++ fetch-db.js
@@ -1,4 +1,9 @@
 const fs = require('fs')
 const request = require('request')
+
+const { generateGradesFile } = require('./util/services')
+
 request('https://outlier-coding-test-data.onrender.com/students.db').pipe(fs.createWriteStream('students.db'))
 console.log('Fetching students.db...')
+
+generateGradesFile()
---
+++ util/services.js
@@ -0,0 +1,37 @@
+const axios = require('axios')
+const fs = require('fs').promises
+const path = require('path')
+
+const gradesUrl = 'https://outlier-coding-test-data.onrender.com/grades.json'
+const FOLDER = 'json_data'
+const FILE = 'course-grades-data.json'
+
+async function generateGradesFile() {
+    try {
+        const filePath = path.join(FOLDER, FILE)
+        try {
+            return JSON.parse(await fs.readFile(filePath, 'utf8'))
+        } catch (readError) {
+            const gradesData = await getStudentsGradesData()
+            try {
+                await fs.mkdir(FOLDER, { recursive: true })
+                await fs.writeFile(filePath, JSON.stringify(gradesData, null, 2), 'utf8')
+                console.log('Grades data successfully downloaded')
+            } catch (writeError) {
+                console.error(`Error creating file ${filePath}:`, writeError)
+            }
+        }
+    } catch (error) {
+        throw new Error(`Error fetching data: ${error.message}`)
+    }
+}
+
+async function getStudentsGradesData() {
+    try {
+        const response = await axios.get(gradesUrl)
+        const jsonData = response.data
+        return jsonData
+    } catch (error) {
+        throw new Error(`Error fetching data: ${error.message}`)
+    }
+}
